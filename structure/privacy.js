c4f460046f5a9f5530ff5b1255a9d89d179c2776
adriabama06 <adriabama06@gmail.com>
2024-11-20 16:01:40 +0100

    [FEAT] Add privacy multi account, NOT TESTED

--- structure/privacy.js
+++ structure/privacy.js
@@ -1,19 +1,45 @@
 const Express = require("express");
 const express = Express.Router();
 const fs = require("fs");
-const privacy = require("./../responses/privacy.json");
+
+const LAWINSERVER_PATH = process.env.LAWINSERVER ?? "LawinServer";
+const preProcessRequest = (req, filename) => {
+    const { accountId } = req.params;
+
+    const ProfilesFolder = path.join(LAWINSERVER_PATH, accountId, "responses");
+
+    if(!fs.existsSync(ProfilesFolder)) {
+        fs.mkdirSync(ProfilesFolder, { recursive: true });
+    }
+
+    const ProfileFilePath = path.join(ProfilesFolder, filename);
+
+    if(!fs.existsSync(ProfileFilePath)) {
+        fs.copyFileSync(path.join("responses", filename), ProfileFilePath);
+    }
+
+    const profile = JSON.parse(
+        fs.readFileSync(ProfileFilePath)
+    );
+
+    return {accountId, ProfilesFolder, ProfileFilePath, profile};
+}
 
 express.get("/fortnite/api/game/v2/privacy/account/:accountId", async (req, res) => {
+    const { profile: privacy } = preProcessRequest(req, "privacy.json");
+
     privacy.accountId = req.params.accountId;
 
     res.json(privacy);
 })
 
 express.post("/fortnite/api/game/v2/privacy/account/:accountId", async (req, res) => {
+    const { profile: privacy, ProfileFilePath: privacyFilePath } = preProcessRequest(req, "privacy.json");
+
     privacy.accountId = req.params.accountId;
     privacy.optOutOfPublicLeaderboards = req.body.optOutOfPublicLeaderboards;
 
-    fs.writeFileSync("./responses/privacy.json", JSON.stringify(privacy, null, 2));
+    fs.writeFileSync(privacyFilePath, JSON.stringify(privacy, null, 2));
 
     res.json(privacy);
     res.end();
